84a7465f673b44d48c8189d08ce72a1acddf1397
mahindar <kmak8894@gmail.com>
2016-05-02 17:26:04 +0530

    Saddle Code

--- Project/Own/Mesh/MeshVertex.hpp
+++ Project/Own/Mesh/MeshVertex.hpp
@@ -163,6 +163,46 @@ class MeshVertex
     /** Recompute the quadric error matrix for this vertex. */
     void updateQuadric();
 
+     // Me added
+    bool is_saddle(){
+      double angle = 0;
+      for (FaceConstIterator fi = facesBegin(); fi != facesEnd(); ++fi){
+        MeshVertex* v1, v2, v3;
+        std::list<Vertex *>::iterator i1 = fi->verticesBegin();
+        int flag = 0;
+        v1 = *i1;
+        if(v1 == this)flag = 1;
+        i1++;
+        v2 = *i1;
+        if(v2 == this)flag = 2;
+        i1++;
+        v3 = *i1;
+        if(v3 == this)flag = 3;
+
+        Vector3 v1position = v1->getPosition();
+        Vector3 v2position = v2->getPosition();
+        Vector3 v3position = v3->getPosition();
+        if(flag == 1){
+          Vector3 p1 = v2position - v1position;
+          Vector3 p2 = v3position - v1position;
+          angle += radiansToDegrees(fastArcCos((p1.dot(p2))/(p1.length()*p2.length())));
+        }else if(flag == 2){
+          Vector3 p1 = v1position - v2position;
+          Vector3 p2 = v3position - v2position;
+          angle += radiansToDegrees(fastArcCos((p1.dot(p2))/(p1.length()*p2.length())));
+        }else if(flag == 3){
+          Vector3 p1 = v1position - v3position;
+          Vector3 p2 = v2position - v3position;
+          angle += radiansToDegrees(fastArcCos((p1.dot(p2))/(p1.length()*p2.length())));
+        }
+      }
+      return (angle > 360.0);
+    }
+
+    bool is_boundary_saddle(){
+      return is_saddle() || isBoundary();
+    }
+
   private:
     friend class Mesh;
 
